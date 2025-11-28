98d6793dce03365e83f2c57d05b635fd40aa8656
Sreekanth <prsreekanth920@gmail.com>
2025-11-28 19:56:32 +0530

    Metadata API changes
    Signed-off-by: Sreekanth <prsreekanth920@gmail.com>
    Signed-off-by: vtiwari5 <vaibhav_tiwari1@intuit.com>

---
+++ examples/mapper/mapper.ts
@@ -0,0 +1,42 @@
+import { map } from '../../index.js'
+const { MapServer, Message } = map
+
+async function mapFn(datum: map.Datum): Promise<map.Message[]> {
+  console.log(`mapFn: ${JSON.stringify(datum)}`)
+  const key = datum.keys[0] ?? 'default-key'
+  const value = datum.value ?? Buffer.from('default-value')
+  if (value.toString() === 'bad') {
+    console.log('Dropping message')
+    return [Message.toDrop()]
+  }
+
+  let userMetadataGroups = datum.userMetadata?.getGroups() ?? []
+  console.log(`userMetadataGroups: ${JSON.stringify(userMetadataGroups)}`)
+
+  const userMetadata = new map.UserMetadata()
+  userMetadata.addKv('custom-group', 'custom-key', Buffer.from('custom-value'))
+
+  return [
+    {
+      keys: [key],
+      value: value,
+      userMetadata,
+    },
+  ]
+}
+
+async function main() {
+  console.log('Starting mapper')
+  const server = new MapServer(mapFn)
+
+  const shutdown = () => {
+    server.stop()
+  }
+  process.on('SIGTERM', shutdown)
+  process.on('SIGINT', shutdown)
+
+  await server.start('/tmp/mapper.sock', '/tmp/mapper.info')
+  console.log('Mapper finished')
+}
+
+main().catch(console.error)
