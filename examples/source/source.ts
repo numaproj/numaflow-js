ddc547e68e5bfe4f3b74694cfed67f0227b1cdc1
vtiwari5 <vaibhav_tiwari1@intuit.com>
2025-12-03 11:01:54 -0500

    Working example for UD source
    Signed-off-by: vtiwari5 <vaibhav_tiwari1@intuit.com>

---
+++ examples/source/source.ts
@@ -0,0 +1,60 @@
+import { source } from '../../index'
+
+class Sourcer implements source.Sourcer {
+    counter = 0
+    partitionIdx = 0
+
+    async *read(requests: source.ReadRequest): AsyncIterableIterator<source.Message> {
+        for (let i = 1; i <= requests.numRecords; i++) {
+            const payload = Buffer.from(`message-${this.counter}`, 'utf-8')
+
+            const offset = {
+                offset: Buffer.from(`${this.counter}`, 'utf-8'),
+                partitionId: this.partitionIdx,
+            }
+
+            const message = {
+                payload: payload,
+                offset: offset,
+                eventTime: new Date(Date.now()),
+                keys: ['key1'],
+                headers: { source: 'simple' },
+            }
+
+            this.counter += 1
+
+            yield message
+        }
+    }
+
+    async ack(offsets: source.Offset[]): Promise<void> {
+        console.log('ack', offsets)
+    }
+
+    async nack(offsets: source.Offset[]): Promise<void> {
+        console.log('nack', offsets)
+    }
+
+    async pending(): Promise<number | null> {
+        return 0
+    }
+
+    async partitions(): Promise<number[] | null> {
+        return [this.partitionIdx]
+    }
+}
+
+async function main() {
+    const server = new source.SourceAsyncServer(new Sourcer())
+
+    const shutdown = () => {
+        server.stop()
+    }
+    process.on('SIGTERM', shutdown)
+    process.on('SIGINT', shutdown)
+
+    console.log('Starting source async server')
+    await server.start()
+}
+
+main().catch(console.error)
